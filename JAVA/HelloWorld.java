c2ef3cbe28ac49a3528f23419346b8efcbce9ac4
Sagar Tamang <sagarmoktan2369@gmail.com>
2026-08-18 12:49:08 +0545

    early javas

---
+++ JAVA/HelloWorld.java
@@ -0,0 +1,50 @@
+// import java.applet.Applet;
+// import java.awt.Graphics;
+
+// public class HelloWorld extends Applet {
+//     @Override
+//     public void paint(Graphics g) {
+//         g.drawString("Hello World", 20, 20);
+//     }
+// }
+
+// public class HelloWorld{
+//     public static void main(String[] args){
+//         System.out.println("Sagar Moktan is my name and I am a very good person");
+//         int am= 5000;
+//         String name = "Lal Kumar Tamang";
+//         System.out.println("HEllo world...");
+//     }
+// }
+
+
+//type casting in java...
+
+// public class HelloWorld{
+//     public static void main(String[] args){
+//         int a = 55;
+//         byte b = 127;
+//         byte k = (byte) a;
+//         float f = 54.3f;
+//         int ss = (int)f;
+//         System.out.println(ss);
+//         System.out.println(b);
+
+//     }
+// }
+
+// arithmetic operators in java 
+
+// public class HelloWorld{
+//     public static void main(String[] args) {
+//         int num1 = 5;
+//         int num2 = 3;
+//         int result = num1 / num2;
+//         num1 = num1 + 2;
+//         System.out.println(num1);
+//         System.out.println(result);
+//     }
+// }
+
+// relational operators in java 
+
