eb879c20370dbfe103f1a904b9e6b9333cb2cd26
Sagar Tamang <sagarmoktan2369@gmail.com>
2026-08-18 22:12:52 +0545

    some more files with sql..

---
+++ JAVA/Main.java
@@ -0,0 +1,50 @@
+// public class day1{
+//     public static void main(String[] args){
+//         System.out.println("Sagar Moktan.....");
+//     }
+// }
+
+
+// public class Main{
+//     public static void main(String[] args){
+//         int num1 = 4;
+//         int num2 = 5;
+//         int result = num1 + num2;
+//         System.out.print(result);
+
+//     }
+// }
+
+// data types 
+// primitive = inbuilt variables provided by java, its typese are:
+// - integer subtypes--> byte, short, int, long
+// -float --> double, float
+// -character --> 2 bytes 
+// -boolean --> only true or false , not 1 and 0
+
+// class Main{
+//     public static void main(String[] args){
+//         int num1 = 5;
+//         byte by = 23;
+//         short sh = 3434;
+//         long l = 34343434;
+
+//         float f = 33.3f;
+//         double dd = 5.33;
+
+//         char c = 'S';  // only '' is used in java for characters not ""
+
+//         boolean b = true;
+//         System.out.println(num1 +" "+ by+" "+ sh +" "+ l+ " "+ f+" "+ dd+" "+ c+" "+ b );
+//     }
+// }
+
+class Main{
+    public static void main(String[] args){
+        int a = 33;
+        byte by = 22;
+        by = (byte)a;
+        System.out.print(by);
+    }
+}
+
---
+++ SQL/sqd1.sql
@@ -0,0 +1,13 @@
+-- CREATE DATABASE moktan;
+USE moktan;
+-- CREATE TABLE family (
+-- 	mem_id INT PRIMARY KEY,
+-- 	f_name VARCHAR(50),
+--     l_name VARCHAR(50),
+--     p_num VARCHAR(50),
+--     b_day DATETIME
+-- );
+
+-- ALTER TABLE family ADD age INT;
+
+-- SELECT * FROM family;
